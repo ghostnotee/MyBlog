b744fd3490fcf50a582dab041e0c464d7208d528
ghostNoté <selcukbilgen@hotmail.com>
2024-10-05 00:23:48 +0300

    validation rules added to models

--- Data.Models/BlogPost.cs
+++ Data.Models/BlogPost.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Data.Models;
 
 public class BlogPost
 {
     public string? Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Text { get; set; } = string.Empty;
+    [Required] [MinLength(5)] public string Title { get; set; } = string.Empty;
+    [Required] public string Text { get; set; } = string.Empty;
     public DateTime PublishDate { get; set; }
     public Category? Category { get; set; }
     public List<Tag> Tags { get; set; } = new();
--- Data.Models/Category.cs
+++ Data.Models/Category.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Data.Models;
 
 public class Category
 {
     public string? Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    [Required] public string Name { get; set; } = string.Empty;
 }
