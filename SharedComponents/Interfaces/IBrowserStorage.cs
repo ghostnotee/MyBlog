ce58c1c67d4f52046d796da2b332db045cca824b
ghostNoté <selcukbilgen@hotmail.com>
2024-10-30 17:47:27 +0300

    Implementing Browser Storage wip.

---
+++ SharedComponents/Interfaces/IBrowserStorage.cs
@@ -0,0 +1,8 @@
+namespace SharedComponents.Interfaces;
+
+public interface IBrowserStorage
+{
+    Task<T?> GetAsync<T>(string key);
+    Task SetAsync(string key, object value);
+    Task DeleteAsync(string key);
+}
